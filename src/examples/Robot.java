06563bd85e8dccc995dd30e22af292e9e07cd27f
Alexander Hartson <alexander-hartson@live.com>
2018-05-07 19:38:02 -0500

    adding files

---
+++ src/examples/Robot.java
@@ -0,0 +1,25 @@
+package examples;
+
+public class Robot {
+
+    private int MaxSpeed;
+
+    public Robot() {
+        this.MaxSpeed = 1;
+    }
+
+    public int getMaxSpeed() {
+        return MaxSpeed;
+    }
+
+    public void setMaxSpeed(int MaxSpeed) {
+        if (MaxSpeed >= 0)
+            this.MaxSpeed = MaxSpeed;
+        else
+            this.MaxSpeed = 0;
+    }
+
+    public void travel() {
+        System.out.println("The robot moves a distance of " + MaxSpeed + " units.");
+    }
+}
